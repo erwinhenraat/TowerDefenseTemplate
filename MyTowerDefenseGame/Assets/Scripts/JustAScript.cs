71318e0f58b74c9395aa6a3daa6e7da72fb2706b
erwin henraat <e.henraat@ma-web.nl>
2023-06-28 16:28:18 +0200

    added sln and edited unity project scene

--- MyTowerDefenseGame/Assets/Scripts/JustAScript.cs
+++ MyTowerDefenseGame/Assets/Scripts/JustAScript.cs
@@ -1,10 +1,24 @@
-﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-
-public class JustAScript 
+public class JustAScript : MonoBehaviour
 {
+    private Rigidbody r;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Rigidbody>();
+    }
 
-	public JustAScript()
-	{
-	}
+    // Update is called once per frame
+    void Update()
+    {
+                
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        r.velocity= Vector3.up * force;
+    }
 }
